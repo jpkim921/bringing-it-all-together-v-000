662eae76d3b796621551310da77ac3db39919014
Jayon Kim <jpkim921@gmail.com>
2018-05-06 04:40:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -106,23 +106,4 @@ class Dog
     # binding.pry
  end
 
- # def self.find_or_create_by(name:, breed:)
- #   sql = <<-SQL
- #    SELECT *
- #    FROM dogs
- #    WHERE name = name
- #    AND breed = breed
- #   SQL
- #
- #    dog = DB[:conn].execute(sql)
- #    if !dog.empty?
- #      dog_data = dog[0]
- #      dog = Dog.new_from_db(dog_data)
- #      # dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
- #    else
- #      dog = self.create(name: name, breed: breed)
- #    end
- #    dog
- #  end
-
 end
