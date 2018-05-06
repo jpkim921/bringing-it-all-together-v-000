776b598c148a09967f36dd0f7c4edf7dee4b9f73
Jayon Kim <jpkim921@gmail.com>
2018-05-06 03:25:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,11 +25,11 @@ class Dog
 
   def self.find_by_id(num)
     sql = <<-SQL
-    SELECT * 
-    FROM dogs 
+    SELECT *
+    FROM dogs
     WHERE id = num
     SQL
-    
+
     DB[:conn].execute(sql)
     binding.pry
   end
